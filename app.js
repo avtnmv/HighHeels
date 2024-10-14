00ad0ce4d135b40cf5c1cb365e3c32d33461e6ff
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-10-14 16:49:40 +0300

    feat: add faq section

---
+++ app.js
@@ -0,0 +1,83 @@
+function look() {
+    var param = document.getElementById('div1');
+    var svgIcon = document.querySelector('.svg-icon'); 
+  
+    
+    if (param.style.display == "none") {
+        param.style.display = "block";
+        svgIcon.classList.add('rotated'); 
+    } else {
+        param.style.display = "none";
+        svgIcon.classList.remove('rotated'); 
+    }
+  }
+  
+  
+  function look2() {
+    var param = document.getElementById('div2');
+    var svgIcon2 = document.querySelector('.svg-icon2'); 
+  
+    
+    if (param.style.display == "none") {
+        param.style.display = "block";
+        svgIcon2.classList.add('rotated'); 
+    } else {
+        param.style.display = "none";
+        svgIcon2.classList.remove('rotated'); 
+    }
+  }
+  
+  function look3() {
+    var param = document.getElementById('div3');
+    var svgIcon3 = document.querySelector('.svg-icon3'); 
+  
+    
+    if (param.style.display == "none") {
+        param.style.display = "block";
+        svgIcon3.classList.add('rotated'); 
+    } else {
+        param.style.display = "none";
+        svgIcon3.classList.remove('rotated'); 
+    }
+  }
+  function look4() {
+    var param = document.getElementById('div4');
+    var svgIcon4 = document.querySelector('.svg-icon4'); 
+  
+    
+    if (param.style.display == "none") {
+        param.style.display = "block";
+        svgIcon4.classList.add('rotated'); 
+    } else {
+        param.style.display = "none";
+        svgIcon4.classList.remove('rotated'); 
+    }
+  }
+  
+  
+  function look5() {
+    var param = document.getElementById('div5');
+    var svgIcon5 = document.querySelector('.svg-icon5'); 
+  
+    
+    if (param.style.display == "none") {
+        param.style.display = "block";
+        svgIcon5.classList.add('rotated'); 
+    } else {
+        param.style.display = "none";
+        svgIcon5.classList.remove('rotated'); 
+    }
+  }
+  function look6() {
+    var param = document.getElementById('div6');
+    var svgIcon2 = document.querySelector('.svg-icon6'); 
+  
+    
+    if (param.style.display == "none") {
+        param.style.display = "block";
+        svgIcon2.classList.add('rotated'); 
+    } else {
+        param.style.display = "none";
+        svgIcon2.classList.remove('rotated'); 
+    }
+  }
